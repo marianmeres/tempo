7e6f1f88ed0eff55bd05ce61e596e1e075f3c34f
Marian Meres <marian@meres.sk>
2023-03-11 16:13:25 +0100

    footer

--- src/lib/components/layout/Footer.svelte
+++ src/lib/components/layout/Footer.svelte
@@ -1,7 +1,5 @@
 <footer class="p-2 text-xs text-center bg-gray-200">
 	<div class="font-light opacity-75 space-x-1">
 		<a href="mailto:marian@meres.sk">marian@meres.sk</a>
-		<span>|</span>
-		<a href="https://github.com/marianmeres/tempo" target="_blank">source</a>
 	</div>
 </footer>
