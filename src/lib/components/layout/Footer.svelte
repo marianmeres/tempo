6bf3b27c0378b2e2d9d45437bd61c679856125d5
Marian Meres <marian@meres.sk>
2023-03-18 12:10:16 +0100

    show version, npm update

--- src/lib/components/layout/Footer.svelte
+++ src/lib/components/layout/Footer.svelte
@@ -1,5 +1,9 @@
+<script>
+	import { page } from '$app/stores';
+</script>
 <footer class="p-2 text-xs text-center bg-gray-200">
 	<div class="font-light opacity-75 space-x-1">
-		<a href="mailto:marian@meres.sk">marian@meres.sk</a>
+		<a href="mailto:marian@meres.sk">marian@meres.sk</a> |
+		v{$page.data?.VERSION}
 	</div>
 </footer>
---
+++ src/routes/+layout.server.ts
@@ -0,0 +1,7 @@
+import fs from "node:fs";
+
+const VERSION = JSON.parse(fs.readFileSync('package.json', 'utf8')).version;
+
+export async function load() {
+	return { VERSION }
+}
